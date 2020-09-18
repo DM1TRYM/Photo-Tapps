5d10ce7247f954e90c5b8d4f7b5e6ca4c6abea5a
Dmitrii <dm1trym2130@gmail.com>
2020-09-18 21:05:47 +0300

    Add files via upload

---
+++ Photo Tapps/Photo Tapps/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Mikhailov on 30.04.2020.
+//  Copyright © 2020 Funtik Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+}
